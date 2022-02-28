1e63539226057e7973d8388505bea6edcfca2519
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 19:41:26 +0100

    ...

--- scripts/tier3GPSReal/caseStudy.sh
+++ scripts/tier3GPSReal/caseStudy.sh
@@ -1,5 +1,10 @@
-bash launchServer.sh &
+java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar reg 9000
+REG=$!
+sleep 
+sudo cgexec -g cpu:3tier --sticky java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar srv 220 500 360 http://127.0.0.1:9000 9001 9002 9003
+SRV=$!
 sleep 10
 bash changeConcurServer.sh $2
 java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar cli $1 140 60000 http://127.0.0.1:9000 ../../data/tier3GPSReal/case-$1-$2.csv
-pkill java
+kill $REG
+kill $SRV

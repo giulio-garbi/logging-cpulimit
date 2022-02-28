7752c4f6a712285bc8a8e0fb72dc4c0c4f14cefd
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 19:16:26 +0100

    Bugfix caseStudy script

--- scripts/tier3GPSReal/caseStudy.sh
+++ scripts/tier3GPSReal/caseStudy.sh
@@ -1,5 +1,5 @@
-bash initServer.sh &
-sleep 1
+bash launchServer.sh &
+sleep 10
 bash changeConcurServer.sh $2
 java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar cli $1 140 60000 http://127.0.0.1:9000 ../../data/tier3GPSReal/case-$1-$2.csv
 pkill java

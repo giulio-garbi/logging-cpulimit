8130fad931a81d8d665595cd3bd04656b5ea1c93
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 19:51:09 +0100

    ...

--- scripts/tier3GPSReal/caseStudy.sh
+++ scripts/tier3GPSReal/caseStudy.sh
@@ -7,3 +7,4 @@ sleep 10
 bash changeConcurServer.sh $2
 java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar cli $1 140 60000 http://127.0.0.1:9000 ../../data/tier3GPSReal/case-$1-$2.csv
 sudo pkill java
+sleep 10

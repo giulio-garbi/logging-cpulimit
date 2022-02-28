f98f9c5881ec3acb87e5ad62e85f85a383daffe9
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 18:39:53 +0100

    Launch server

--- scripts/tier3GPSReal/launchServer.sh
+++ scripts/tier3GPSReal/launchServer.sh
@@ -1,2 +1,2 @@
 java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar reg 9000 &
-sudo cgexec -g cpu:3tier --sticky java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar srv 22 50 36 http://127.0.0.1:9000 9001 9002 9003
+sudo cgexec -g cpu:3tier --sticky java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar srv 220 500 360 http://127.0.0.1:9000 9001 9002 9003

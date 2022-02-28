6aaa5ecaae6419532ede39c59ae999658c5a2fb7
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 19:13:17 +0100

    Self contained caseStudy script

--- scripts/tier3GPSReal/caseStudy.sh
+++ scripts/tier3GPSReal/caseStudy.sh
@@ -1,5 +1,5 @@
 bash initServer &
-sleep 1
+sleep 10
 bash changeConcurServer.sh $2
 java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar cli $1 140 60000 http://127.0.0.1:9000 ../../data/tier3GPSReal/case-$1-$2.csv
 pkill java

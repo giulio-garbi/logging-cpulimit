cfb2a7dd85321fae01ec517f255870f1941a2817
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 18:56:45 +0100

    changed script invocation

--- scripts/tier3GPSReal/caseStudy.sh
+++ scripts/tier3GPSReal/caseStudy.sh
@@ -1,2 +1,2 @@
-./changeConcurServer.sh $2
+bash changeConcurServer.sh $2
 java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar cli $1 140 60000 http://127.0.0.1:9000 ../../data/tier3GPSReal/case-$1-$2.csv

844d7dd2fcd6f98d9ebd2fbacb7b18df5f890853
Giulio Garbi <julius.grb@gmail.com>
2022-02-28 18:36:17 +0100

    Launch server script with explicit registry

--- scripts/tier3GPSReal/launchServer.sh
+++ scripts/tier3GPSReal/launchServer.sh
@@ -1,1 +1,2 @@
-sudo cgexec -g cpu:3tier --sticky java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar srv 22 50 36 9000 9001 9002 9003
+java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar reg 9000
+sudo cgexec -g cpu:3tier --sticky java -Djava.compiler=NONE -jar ../../data/tier3GPSReal/three_tier.jar srv 22 50 36 http://127.0.0.1:9000 9001 9002 9003
